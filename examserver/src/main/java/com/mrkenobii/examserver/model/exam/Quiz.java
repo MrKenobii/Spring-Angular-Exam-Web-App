0504a56536980df70702b0c68286fb290e0c4fc4
MrKenobii <anldygc35@windowslive.com>
2022-10-03 19:06:36 +0300

    Category Quiz and Question entities

---
+++ examserver/src/main/java/com/mrkenobii/examserver/model/exam/Quiz.java
@@ -0,0 +1,115 @@
+package com.mrkenobii.examserver.model.exam;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "quiz")
+public class Quiz {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String title;
+    private String description;
+    private String maxMarks;
+    private String numberOfQuestions;
+    private boolean active = false;
+    @ManyToOne(fetch = FetchType.EAGER)
+    private Category category;
+    @OneToMany(mappedBy = "quiz", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JsonIgnore
+    private Set<Question> questions = new HashSet<>();
+
+    public Quiz() {
+    }
+
+    public Quiz(Long id, String title, String description, String maxMarks, String numberOfQuestions, boolean active, Category category, Set<Question> questions) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.maxMarks = maxMarks;
+        this.numberOfQuestions = numberOfQuestions;
+        this.active = active;
+        this.category=category;
+        this.questions=questions;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getMaxMarks() {
+        return maxMarks;
+    }
+
+    public void setMaxMarks(String maxMarks) {
+        this.maxMarks = maxMarks;
+    }
+
+    public String getNumberOfQuestions() {
+        return numberOfQuestions;
+    }
+
+    public void setNumberOfQuestions(String numberOfQuestions) {
+        this.numberOfQuestions = numberOfQuestions;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public Set<Question> getQuestions() {
+        return questions;
+    }
+
+    public void setQuestions(Set<Question> questions) {
+        this.questions = questions;
+    }
+
+    @Override
+    public String toString() {
+        return "Quiz{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", maxMarks='" + maxMarks + '\'' +
+                ", numberOfQuestions='" + numberOfQuestions + '\'' +
+                ", active=" + active +
+                '}';
+    }
+}
